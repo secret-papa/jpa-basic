19228f6568c4f86125a66ea433ba3c0be453ed7b
socar-dep <dep@socar.kr>
2025-04-16 09:22:32 +0900

    connect jpa

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,16 @@
+package hellojpa;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+        emf.close();
+    }
+}
