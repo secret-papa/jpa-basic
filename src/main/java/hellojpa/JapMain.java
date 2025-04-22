4064c77eec421476ab55cc74b377a1ac53a60981
socar-dep <dep@socar.kr>
2025-04-22 09:46:13 +0900

    연관관계 매핑

--- src/main/java/hellojpa/JapMain.java
+++ src/main/java/hellojpa/JapMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
+import hellojpa.domain.Team;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -16,6 +18,23 @@ public class JapMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("TeamA");
+
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeamId(team.getId());
+
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = em.find(Team.class, findMember.getTeamId());
+
+            System.out.println("findTeam = " + findTeam);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -12,10 +12,6 @@ public class Member {
     @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
-    private String name;
-    private String city;
-    private String street;
-    private String zipcode;
 
     public Long getId() {
         return id;
@@ -25,35 +21,25 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public String getCity() {
-        return city;
+    public Long getTeamId() {
+        return teamId;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
     }
 
-    public String getStreet() {
-        return street;
-    }
+    @Column(name = "USERNAME")
+    private String username;
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
+    @Column(name = "TEAM_ID")
+    private Long teamId;
 }
