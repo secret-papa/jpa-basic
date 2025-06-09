d787c096ea7deac7188bc17fd5dc1353a3eaaacd
socar-dep <dep@socar.kr>
2025-06-09 09:38:34 +0900

    JPQL

--- src/main/java/hellojpa/JapMain.java
+++ src/main/java/hellojpa/JapMain.java
@@ -16,43 +16,10 @@ public class JapMain {
 
         try {
 
-            Member member = new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("======================START======================");
-            Member findMember = em.find(Member.class, member.getId());
-
-//            List<Address> addressHistory = findMember.getAddressHistory();
-//            for (Address address : addressHistory) {
-//                System.out.println("address = " + address.getCity());
-//            }
-//
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-//            for (String favoriteFood : favoriteFoods) {
-//                System.out.println("favoriteFood = " + favoriteFood);
-//
-
-//            Address add = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", add.getStreet(), add.getZipcode()));
-//
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-
-//            findMember.getAddressHistory().remove((new AddressEntity("old1", "street", "10000")));
-//            findMember.getAddressHistory().add((new AddressEntity("newCity1", "street", "10000")));
+            List<Member> resultList = em.createQuery(
+                    "SELECT m From Member m WHERE m.name LIKE '%kim%'",
+                    Member.class
+            ).getResultList();
 
             tx.commit();
         } catch (Exception e) {
