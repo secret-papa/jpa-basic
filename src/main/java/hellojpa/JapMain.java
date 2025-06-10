75e98807949ad53e1ded7c46b04121f1914a9d2e
socar-dep <dep@socar.kr>
2025-06-10 09:15:02 +0900

    JPQL Pagination

--- src/main/java/hellojpa/JapMain.java
+++ src/main/java/hellojpa/JapMain.java
@@ -4,7 +4,6 @@ import hellojpa.domain.*;
 import jakarta.persistence.*;
 
 import java.util.List;
-import java.util.Set;
 
 public class JapMain {
     public static void main(String[] args) {
@@ -15,43 +14,26 @@ public class JapMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setName("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-//            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = em.createQuery(
-                    "select m from Member m",
-                        Member.class
-                    )
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-            Member findMember = resultList.get(0);
-            findMember.setAge(11);
+            System.out.println("resultList = " + resultList.size());
 
-//            결과가 하나인 경우
-//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id = 10", Member.class);
-//            query.getSingleResult();
-
-            for (Member member1: resultList) {
-                System.out.println("member1 = " + member1);
+            for (Member m : resultList) {
+                System.out.println("m = " + m.getAge());
             }
-
-//            Type을 정의할 수 없을 경우 new를 활용할 수 있다
-            List<MemberDto> resultList1 = em.createQuery(
-                            "select new hellojpa.domain.MemberDto(m.name, m.age) from Member m",
-                            MemberDto.class
-                    )
-                    .getResultList();
-
-            MemberDto memberDto = resultList1.get(0);
-            System.out.println("memberDto.name = " + memberDto.getName());
-            System.out.println("memberDto.age = " + memberDto.getAge());
-
 
             tx.commit();
         } catch (Exception e) {
